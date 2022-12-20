b7320012324849d2872a1689a4a075cb6941aa69
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-12-20 19:49:38 +0100

    adds table with hotels

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php hotel</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <main class="container">
+        <div>
+            <h1>PHP hotels</h1>
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>name:</th>
+                    <th>description:</th>
+                    <th>parking:</th>
+                    <th>vote:</th>
+                    <th>distance to center:</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach($hotels as $hotel){
+                        echo "<tr>";
+                        echo "<td>{$hotel ['name']}</td>";
+                        echo "<td>{$hotel ['description']}</td>";
+                        echo "<td>{$hotel ['parking']}</td>";
+                        echo "<td>{$hotel ['vote']}</td>";
+                        echo "<td>{$hotel ['distance_to_center']}</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+            
+        </table>
+
+    </main>
+
+    
+    
+</body>
+</html>
